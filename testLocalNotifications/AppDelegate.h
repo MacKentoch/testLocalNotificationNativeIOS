79162a431250aad02a9cfee940eef1a48c4587d9
MacKentoch <erwan.datin@gmail.com>
2016-09-09 21:06:16 +0200

    Initial Commit

---
+++ testLocalNotifications/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testLocalNotifications
+//
+//  Created by MacKentoch on 09/09/2016.
+//  Copyright © 2016 Erwan DATIN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testLocalNotifications/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testLocalNotifications
+//
+//  Created by MacKentoch on 09/09/2016.
+//  Copyright © 2016 Erwan DATIN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
